94b48aca0aad346b52e09729414bd074f36d6ce0
Luiz Gabriel <luizgpp@gmail.com>
2019-03-12 19:26:56 -0300

    Episode 4 - Blade Layout Files

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+@section('title', 'About Us')
+
+@section('content')
+    <h1>About Us</h1>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit veniam sint, fuga quas id, harum, dolore laboriosam modi sunt soluta excepturi nobis error! Deleniti assumenda et pariatur at delectus illum!</p> 
+@endsection
+
