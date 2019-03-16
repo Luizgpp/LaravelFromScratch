8dc2dddd07697efd16a914e63ed3ffb0ba1cae18
luizgpp <luizgpp@gmail.com>
2019-03-15 22:18:29 -0300

    Episode 16 - Your First Eloquent Relationships

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -4,9 +4,21 @@
     
 @section('content')
 
-    <h1 class="title">{{$project->title}}</h1>   
-    <div class="content">{{$project->description}}</div>
+    <h1 class="title is-inline-block">{{$project->title}}</h1> 
+      
+    <a class="button is-link is-outlined" href="/projects/{{$project->id}}/edit">edit</a>
 
-    <a class="button" href="/projects/{{$project->id}}/edit">edit</a>
+    <div>{{$project->description}}</div>
+
+    <h2 class="subtitle">Tasks</h2>
+
+    <ul>
+        @if($project->tasks->count())
+            @foreach ($project->tasks as $task)
+                <li>{{$task->description}}</li>    
+            @endforeach            
+        @endif
+    </ul>
+
     
 @endsection
